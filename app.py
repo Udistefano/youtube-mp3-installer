cb197ae08862ed94d791b09afe846664d8905a27
Udistefano <udistefano@fi.uba.ar>
2026-03-04 01:13:37 -0300

    fix puerto railway

--- app.py
+++ app.py
@@ -106,4 +106,5 @@ def descargar_como_mp3():
 # ARRANCAR EL SERVIDOR
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, threaded=True, host='0.0.0.0', port=port)
